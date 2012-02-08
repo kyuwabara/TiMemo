e018922181035eb24bd5673d4011e5b964f4ab6e
Yu Kuwabara <kyuwabara@gmail.com>
2012-02-08 15:20:39 +0900

    Changed to follow the style of CommonJS.

--- TiMemo/Resources/spec/tests.js
+++ TiMemo/Resources/spec/tests.js
@@ -1,15 +1,16 @@
 exports.execute = function() {
-    var jasmine = require('/spec/lib/jasmine-1.1.0');
-    Ti.include('/spec/lib/jasmine-titanium.js');
+    var jasmine = require('spec/lib/jasmine-1.1.0').jasmine;
+    var tirep = require('spec/lib/jasmine-titanium').TitaniumReporter;
+    jasmine.TitaniumReporter = tirep;
 
     // Include all the test files
-    Ti.include('/spec/test_main.js');
+    //Ti.include('/spec/test_main.js');
 
     for (name in jasmine) {
         Titanium.API.debug(name);
     }
 
-    jasmine.jasmine.getEnv().addReporter(new jasmine.TitaniumReporter());
-    jasmine.jasmine.getEnv().execute();
+    jasmine.getEnv().addReporter(new jasmine.TitaniumReporter());
+    jasmine.getEnv().execute();
 };
 
